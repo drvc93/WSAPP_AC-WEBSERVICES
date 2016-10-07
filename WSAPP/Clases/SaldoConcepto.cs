cae4f8b2577403d4945ad95e88cb3211b30428c9
drvc93 <drvc_1993@hotmail.com>
2016-10-07 04:47:44 -0500

    06-10-2016 modificado

---
+++ WSAPP/Clases/SaldoConcepto.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WSAPP.Clases
+{
+    public class SaldoConcepto
+    {
+
+        private string codConcepto;
+        private string saldoxConcepto;
+        private string pagadoxConceto;
+
+        public SaldoConcepto() { }
+
+        public SaldoConcepto(string Cod , string saldo , string pago)
+        {
+
+            this.codConcepto = Cod;
+            this.saldoxConcepto = saldo;
+            this.pagadoxConceto = pago;
+        }
+        public string CodConcepto
+        {
+            get
+            {
+                return codConcepto;
+            }
+
+            set
+            {
+                this.codConcepto = value;
+            }
+        }
+
+        public string SaldoxConcepto
+        {
+            get
+            {
+                return saldoxConcepto;
+            }
+
+            set
+            {
+                this.saldoxConcepto = value;
+            }
+        }
+
+        public string PagadoxConceto
+        {
+            get
+            {
+                return pagadoxConceto;
+            }
+
+            set
+            {
+                this.pagadoxConceto = value;
+            }
+        }
+    }
+}
